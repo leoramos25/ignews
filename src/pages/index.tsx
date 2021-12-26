3f0cc0030d66908bdc840448bd204addbae1a865
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-12-26 10:54:31 -0300

    Refactoring home page and add home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Next.JS</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          👋<span> Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publication <br />
+            <span>for $9.90 monthy.</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
