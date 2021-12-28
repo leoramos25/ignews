d86e87fc11c1d90cbc7afec5375efd917cecd32c
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-12-28 12:30:04 -0300

    Github OAuth provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,25 @@
 import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi'
+import { FiX } from 'react-icons/fi';
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const { data: session } = useSession();
+  return session ? (
     <button
       className={styles.signInButton}
       type="button"
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Leonardo Ramos
+      {session.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       className={styles.signInButton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sign with GitHub
