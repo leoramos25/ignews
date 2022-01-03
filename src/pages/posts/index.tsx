8f2a525f733218f4cc7eeb2854eb384abc197079
Leonardo Ramos <leoramosdsousa@gmail.com>
2022-01-03 17:51:14 -0300

    consuming data of prismicio

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
 
 export default function Posts() {
   return (
@@ -28,4 +31,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100
+  })
+
+  console.log(response)
+
+  return {
+    props: {}
+  }
 }
