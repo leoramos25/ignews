cc642d73f6a0c21fdf14f3e9bae45774f6af729d
Leonardo Ramos <leoramosdsousa@gmail.com>
2022-01-03 18:52:24 -0300

    listing posts from the prismic on screen

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  exerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,21 +24,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>03 de janeiro de 2022</time>
-            <strong>Titulo qualquer para teste</strong>
-            <p>paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste </p>
-          </a>
-          <a href='#'>
-            <time>03 de janeiro de 2022</time>
-            <strong>Titulo qualquer para teste</strong>
-            <p>paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste </p>
-          </a>
-          <a href='#'>
-            <time>03 de janeiro de 2022</time>
-            <strong>Titulo qualquer para teste</strong>
-            <p>paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste paragrafo qualquer para teste </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.exerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -43,9 +47,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   })
 
-  console.log(response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      exerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
