4a2ad23e9f7860e2d9ade156bad3e98088ac3f3e
Leonardo Ramos <leoramosdsousa@gmail.com>
2022-01-03 20:14:07 -0300

    menu nav

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -52,7 +52,7 @@ export const getStaticProps: GetStaticProps = async () => {
       slug: post.uid,
       title: RichText.asText(post.data.title),
       exerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      updatedAt: new Date(post.last_publication_date as string).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
         year: 'numeric'
